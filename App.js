3ddc29894b1d7ac7dac4bbaee15b4387b9bee86e
Daniel-Merchan <daniel.merchan1910@gmail.com>
2023-02-16 22:37:12 -0500

    first commit

---
+++ App.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+const port = 3001;
+const API_KEY = '2f5ae96c-b558-4c7b-a590-a501ae1c3f6c';
+
+
+app.use(bodyParser.json());
+
+app.post('/DevOps', (req, res) => {
+
+  const apiKey = req.get('X-API-Key');
+  if (apiKey !== API_KEY) {
+    res.status(401).json({ message: 'ERROR: Unauthorized' });
+    return;
+  }
+
+
+  const { message, to, from, timeToLifeSec } = req.body;
+
+  console.log(req.body)
+
+  // Log the received data for debugging purposes
+  console.log(`Received POST request with message "${message}", to "${to}", from "${from}", and TTL ${timeToLifeSec} seconds`);
+
+
+  res.json({ message: `Hello "${to}" your message will be send` });
+});
+
+
+app.all('/DevOps', (req, res) => {
+  res.status(405).json({ message: 'ERROR: Method Not Allowed' });
+});
+
+app.listen(port, () => {
+  //console.log(`Server listening at http://localhost:${port}`);
+});
